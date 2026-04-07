f6d2bd15d68e64181ad7dc68392a7885b9f3e155
Tin Pasco <68724939+KCOntengco@users.noreply.github.com>
2026-04-07 08:26:43 +0800

    initial code

---
+++ main.js
@@ -0,0 +1,19 @@
+// declare variables
+const u, p;
+u = "admin"; //string data type
+p = "12345"; //string data type
+
+// add the function 
+function verbNoun() {
+    let userNameInput = document.getElementById("UserName").value;
+    let passCodeInput = 
+    let fb = 
+
+    // test by using if-else
+    if (u === userNameInput && p === passCodeInput) {
+        // feedback if username and password are correct
+
+    } else  (u === userNameInput && p === passCodeInput) {
+        // feedback if username and password are incorrect
+    }
+}
